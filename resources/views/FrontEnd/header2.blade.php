d22db149fc9b1bbcdf3636d36811ff491606d789
teammed2022 <raymonddavila1023@gmail.com>
2023-03-20 13:14:52 +0800

    changed project to Laravel

--- resources/views/FrontEnd/header2.blade.php
+++ resources/views/FrontEnd/header2.blade.php
@@ -1,8 +1,30 @@
-    <!-- Main Menu End-->
+    <!-- Header 2 - Cart,login etc.-->
+
+   
+       
 
     <div class="outer-box clearfix">
-                        <!-- Shoppping Car -->
+                        <!-- Login-Register -->
+                        <div class="login-register">
+                        @if (Route::has('login'))
+                                    
+                                    @auth
+                                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                        @endif
+                                    @endauth
+                               
+                            @endif
+                        </div>
+    
+                        <!-- Shoppping Cart -->
+                        
                         <div class="cart-btn">
+                            
                             <a href="shopping-cart.html"><i class="icon flaticon-commerce"></i> <span class="count">2</span></a>
 
                             <div class="shopping-cart">
@@ -36,10 +58,8 @@
                         <div class="search-box">
                             <button class="search-btn"><i class="fa fa-search"></i></button>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                   
+    </div>
 
         <!-- Sticky Header  -->
         <div class="sticky-header">
