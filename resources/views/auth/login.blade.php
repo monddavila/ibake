4e7d6511533ff0aa196152141df9780a6050045d
Gian <gbpmendoza76@gmail.com>
2023-03-24 04:13:11 +0800

    refactored login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,76 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>iBake | Login</title>
 
-        <x-validation-errors class="mb-4" />
+<!-- Header Section -->
+@include('FrontEnd.header')
+@include('FrontEnd.header2')
 
-        @if (session('status'))
+    <!--Page Title-->
+    <section class="page-title" style="background-image:url(https://via.placeholder.com/1920x400)">
+        <div class="auto-container">
+            <h1>My account</h1>
+            <ul class="page-breadcrumb">
+                <li><a href="\">home</a></li>
+                <li>My account</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+
+    <!--Login Section-->
+
+    @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <section class="login-section">
+        <div class="auto-container">
+            <!-- Login Form -->
+            <div class="login-form">
+                <h2>Login</h2>
+                <!--Login Form-->
+                <form method="post" action="{{ route('login') }}">
+                @csrf
+                    <div class="form-group">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    </div>
+                    
+                    <div class="form-group">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                    </div>
+                    
+                    <div class="form-group">
+                    <input class="theme-btn" type="submit" name="submit-form" value="Log in">
+                    </div>
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
+                    <div class="form-group">
+                    <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                    </label>
+                    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
+                    <div class="form-group pass">
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}" class="psw">{{ __('Forgot your password?') }}</a>
+                    @endif
+                    </div>
+                </form>
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+            <!--End Login Form -->  
+        </div>
+    </section>
+    <!--End Login Section-->
+
+<!-- Footer Section -->
+@include('FrontEnd.footer') 
+ 
+</body>
+</html>
